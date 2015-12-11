f1facf31764e950a0b59f101875a241b86a6dd58
joe-re <joe.tialtngo@gmail.com>
2015-12-12 02:32:52 +0900

    add flow annotation: Your first component

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,8 +1,10 @@
+/* @flow */
+
 import React from 'react';
 
 export default class CommentBox extends React.Component {
-  render() {
-    return (
+  render():ReactElement {
+    return(
       <div className="commentBox">
         Hello, world! I am a CommentBox.
       </div>
